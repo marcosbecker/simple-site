5bce8acc4d23d29cf13c249ac1df7cb7e45bf21a
Marcos Becker <marcosbecker@gmail.com>
2016-05-14 14:00:29 -0300

    Script to set class to body

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,38 @@
-(function() {
+/*globals navigator,jQuery,document,console,setTimeout*/
+(function () {
     'use strict';
 
-    jQuery(document).ready(function($) {
+    var isMobile = {
+        Android: function () {
+            return navigator.userAgent.match(/Android/i);
+        },
+        BlackBerry: function () {
+            return navigator.userAgent.match(/BlackBerry/i);
+        },
+        iOS: function () {
+            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+        },
+        Opera: function () {
+            return navigator.userAgent.match(/Opera Mini/i);
+        },
+        Windows: function () {
+            return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
+        },
+        any: function () {
+            return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+        }
+    };
 
+    jQuery(document).ready(function ($) {
+        // Add a class to the body tag after identify the device
+        if (isMobile.any()) {
+            $('body').addClass('mobile');
+
+            if (isMobile.iOS) {
+                $('body').addClass('iphone');
+            }
+        } else {
+            $('body').addClass('desktop');
+        }
     });
 }());
